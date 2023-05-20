5b2430b5101538ba1aed878c4cd69f1e03762395
Diego Loaiza <dfloaiza@gmail.com>
2023-05-19 19:18:01 -0500

    Update VentanaPpal.java

--- Unidad3/ClinicaVet/src/USC/ClinicaVet/UI/VentanaPpal.java
+++ Unidad3/ClinicaVet/src/USC/ClinicaVet/UI/VentanaPpal.java
@@ -1,26 +1,35 @@
 package USC.ClinicaVet.UI;
 
+import USC.ClinicaVet.Model.*;
+
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class VentanaPpal extends JFrame {
 	
 	PanelDatos miPanelDatos;
+	ClinicaVet miClinicaVet;
+	String[] medicos;
+	
 	
 	public VentanaPpal()
 	{
 		this.setSize(800, 600);
-		this.setTitle("Clinica Vet - Reserva de Citas");
-		
-		miPanelDatos = new PanelDatos();	
+		this.setTitle("Mi primer JFrame");
+		this.setBackground(Color.BLACK);
+		miPanelDatos = new PanelDatos();
 		this.add(miPanelDatos);
-	}
+		
+		miClinicaVet = new ClinicaVet();
+		miClinicaVet.CargarEspecialistas("./data/medicos.csv");
+		medicos = (String[]) miClinicaVet.getEspecialistas().toArray();
+	}	
 	
-	
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
-		VentanaPpal miVentana = new VentanaPpal();
-		miVentana.setVisible(true);
-	}
-	
+		VentanaPpal miAplicacion = new VentanaPpal();
+		miAplicacion.setVisible(true);		
+	}	
 
 }
