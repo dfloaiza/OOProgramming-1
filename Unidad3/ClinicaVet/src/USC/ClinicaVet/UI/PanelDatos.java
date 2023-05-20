42b9689a6073f40bc98529a32f5c76e53c25ac8e
Diego Loaiza <dfloaiza@gmail.com>
2023-05-19 20:16:49 -0500

    Update PanelDatos.java

--- Unidad3/ClinicaVet/src/USC/ClinicaVet/UI/PanelDatos.java
+++ Unidad3/ClinicaVet/src/USC/ClinicaVet/UI/PanelDatos.java
@@ -1,45 +1,60 @@
 package USC.ClinicaVet.UI;
 
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.*;
+
 
 import javax.swing.*;
 
-public class PanelDatos extends JPanel {
+
+public class PanelDatos extends JPanel implements ActionListener {
 	
-	JLabel lblEspecialidad, lblMedico, lblMascota, lblFecha;
-	JButton btnReservar;
-	JTextField txtNomMascota, txtFecha;
-	JComboBox<String> cmbEspecialidades, cmbMedico;
-		
-	public PanelDatos()
-	{
-		//Instanciar los elementos de UI:
-		lblEspecialidad = new JLabel("Especialidad:");
-		lblMedico = new JLabel("Medico");
-		lblMascota = new JLabel("Nombre de Mascota");
-		lblFecha = new JLabel("Fecha:");
-		
-		btnReservar = new JButton("Reservar");
-		
-		txtNomMascota = new JTextField();
+	JLabel lblMedicos, lblFecha, lblNomMascota;
+	JTextField txtFecha, txtNomMascota;
+	JComboBox<String> cmbMedicos;
+	JButton botonGuardar;
+	
+	public PanelDatos(VentanaPpal pVentanaPpal)
+	{		
+		//crear elementos de UI
+		lblMedicos = new JLabel("Medicos");
+		lblFecha = new JLabel("Fecha");
+		lblNomMascota = new JLabel("Nombre Mascota");
 		txtFecha = new JTextField();
+		txtNomMascota = new JTextField();
+		cmbMedicos = new JComboBox<String>();
+		botonGuardar = new JButton("Guardar");
 		
-		cmbEspecialidades = new JComboBox<String>();
-		cmbMedico = new JComboBox<String>();
+		//adicionar etiqueta y listener al botón:
+		botonGuardar.addActionListener(this);
+		botonGuardar.setActionCommand("GUARDAR");
 		
-		//establecer layout del panel:
-		this.setLayout(new GridLayout(5, 2));
+		//establecer diagramación del panel:
+		this.setLayout(new GridLayout(4,2));
 		
-		this.add(lblEspecialidad);
-		this.add(cmbEspecialidades);
-		this.add(lblMedico);
-		this.add(cmbMedico);
-		this.add(lblMascota);
-		this.add(txtNomMascota);
+		this.add(lblMedicos);
+		this.add(cmbMedicos);
 		this.add(lblFecha);
 		this.add(txtFecha);
+		this.add(lblNomMascota);
+		this.add(txtNomMascota);	
 		this.add(new JLabel(""));
-		this.add(btnReservar);
 	}
+	
+	//Adicionar la lista de médicos que se cargó desde el modelo en el comboBox
+	public void mostrarMedicos(String[] listaMedicos)
+	{
+		for(String medico: listaMedicos)
+			cmbMedicos.addItem(medico);
+		
+		this.repaint();
+	}
+
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub		
+	}
+	
+	
 
 }
